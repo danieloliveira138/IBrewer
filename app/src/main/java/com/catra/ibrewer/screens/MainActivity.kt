cfa522a3b9c8a44f663a71d74daa82e27cd72bb5
daniel.oliveira <daniel.oliveira@e-deploy.com.br>
2020-12-29 17:56:09 -0300

    App Logo. App colors.

---
+++ app/src/main/java/com/catra/ibrewer/screens/MainActivity.kt
@@ -0,0 +1,14 @@
+package com.catra.ibrewer.screens
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.catra.ibrewer.R
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+    }
+}
--- app/src/main/java/com/catra/ibrewer/screens/splash/SplashActivity.kt
+++ app/src/main/java/com/catra/ibrewer/screens/splash/SplashActivity.kt
@@ -1,13 +1,13 @@
-package com.catra.ibrewer
+package com.catra.ibrewer.screens.splash
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import com.catra.ibrewer.R
 
-class MainActivity : AppCompatActivity() {
+class SplashActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
-
+        setContentView(R.layout.splash_activity)
     }
 }
